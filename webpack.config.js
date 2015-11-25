890aa51d98c7a7becf5ce47ecc255a008272ebf2
Juho Vepsalainen <bebraw@gmail.com>
2015-11-25 11:10:19 +0200

    Set up ExtractTextPlugin
    Now build generates a separate CSS for custom styles.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var Clean = require('clean-webpack-plugin');
 var merge = require('webpack-merge');
@@ -7,7 +8,11 @@ var merge = require('webpack-merge');
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build')
+  build: path.join(__dirname, 'build'),
+  css: [
+    path.join(__dirname, 'app'),
+    path.join(__dirname, 'node_modules/purecss')
+  ]
 };
 
 var common = {
@@ -19,10 +24,6 @@ var common = {
   },
   module: {
     loaders: [
-      {
-        test: /\.css$/,
-        loaders: ['style', 'css']
-      },
       {
         test: /\.jsx?$/,
         loaders: ['babel'],
@@ -40,12 +41,6 @@ var common = {
 if(TARGET === 'start' || !TARGET) {
   module.exports = merge(common, {
     devtool: 'eval-source-map',
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify('development')
-      }),
-      new webpack.HotModuleReplacementPlugin()
-    ],
     devServer: {
       historyApiFallback: true,
       hot: true,
@@ -59,7 +54,22 @@ if(TARGET === 'start' || !TARGET) {
       // to customize
       host: process.env.HOST,
       port: process.env.PORT
-    }
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css'],
+          include: PATHS.css
+        }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('development')
+      }),
+      new webpack.HotModuleReplacementPlugin()
+    ]
   });
 }
 
@@ -73,8 +83,18 @@ if(TARGET === 'build') {
       path: PATHS.build,
       filename: 'bundle.[chunkhash].js',
     },
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('style', 'css'),
+          include: PATHS.css
+        }
+      ]
+    },
     plugins: [
       new Clean([PATHS.build]),
+      new ExtractTextPlugin('styles.[chunkhash].css'),
       new webpack.DefinePlugin({
         // This has effect on the react lib size
         'process.env.NODE_ENV': JSON.stringify('production')
